4cf18b0ee495368b8c7c281c9ff66745e81f4be7
Anacarolinar21 <anacarolinarodriguescarolina2@gmail.com>
2022-03-25 19:51:13 -0300

    finalizar exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double n1,n2,n3, media;
+
+Console.WriteLine("digite o primeiro número");
+n1 = Convert.ToDouble(Console.ReadLine())!;
+
+Console.WriteLine("digite o segundo número");
+n2 = Convert.ToDouble(Console.ReadLine())!;
+
+Console.WriteLine("digite o terceiro número");
+n3 = Convert.ToDouble(Console.ReadLine())!;
+
+media = (n1+n2+n3) / 3;
+
+Console.WriteLine($"media aritimetica:{media:N2}");
